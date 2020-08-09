683fb9bb30ce8c9e0c3211b334bb686fd883cfd8
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2020-08-09 23:00:00 +0400

    Try another layout

--- init.js
+++ init.js
@@ -4,14 +4,19 @@ let emitBigFile = () => {
   let content = ["[@react.component]", "let make = () =>", "  <div>"];
   for (let i = 0; i < 100; i++) {
     content.push(
-      `    <div className=[%tw "c1 c2 c3 c4 c5 c6 c7 c8 c9 c10"] />`
+      `    <div className=[%tw "c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11 c12 c13"] />`
     );
   }
 
   content.push("  </div>;");
   return content.join("\n");
 };
+for (let x = 0; x < 10; x++) {
+  if (!fs.existsSync(`src/${x}`)) {
+    fs.mkdirSync(`src/${x}`);
+  }
 
-for (let i = 0; i < 500; i++) {
-  fs.writeFileSync(`src/Test_${i}.re`, emitBigFile());
+  for (let xx = 0; xx < 50; xx++) {
+    fs.writeFileSync(`src/${x}/Test__${x}__${xx}.re`, emitBigFile());
+  }
 }
